ad1684975d0ee2381e5deac314037ac1de5f0808
Harshaadiga <91590053+Harshaadiga@users.noreply.github.com>
2021-11-19 21:09:15 +0530

    Create assignment6.java

---
+++ SelniumAssignments/Assignments/assignment6.java
@@ -0,0 +1,26 @@
+package SeleniumAssignment;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class assignment6 {
+	@Test
+	public void beforeSuiteTest() {
+		System.out.println("before Suite execution");
+	}
+
+	@BeforeSuite
+	public void before_suite() throws InterruptedException {
+//		System.setProperty("webdriver.chrome.driver", "C:\\sdet\\Selenium Driver\\chromedriver.exe");
+		WebDriverManager.chromedriver().proxy("http://10.98.21.24:8080").setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Thread.sleep(3000);
+		driver.manage().window().maximize();
+		driver.close();
+	}
+}
