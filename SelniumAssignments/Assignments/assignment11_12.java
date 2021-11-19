2a5e22fd765bd0205f8c5f7d0a8cc63f25ef96f3
Harshaadiga <91590053+Harshaadiga@users.noreply.github.com>
2021-11-19 21:15:45 +0530

    Update assignment11_12.java

--- SelniumAssignments/Assignments/assignment11_12.java
+++ SelniumAssignments/Assignments/assignment11_12.java
@@ -38,7 +38,7 @@ public class assignment11_12 {
 		}
 
 //		System.setProperty("webdriver.chrome.driver", "C:\\sdet\\Selenium Driver\\chromedriver.exe");
-		WebDriverManager.chromedriver().proxy("http://10.98.21.24:8080").setup();
+		WebDriverManager.chromedriver().proxy("http://").setup();
 		driver = new ChromeDriver();
 
 		driver.manage().window().maximize();
